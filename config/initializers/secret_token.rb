d32a9eb5c3125cb4c19ffad0fc3a8a6c0ad2a413
Yue Li <yueli@uga.edu>
2015-02-17 12:35:35 -0500

    Initiate the project on Rails 4.0
    Rewrote and redesign the web database application for web users and web
    editors

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ugalileo::Application.config.secret_key_base = 'caded21fb6cae880afc568a976092928c789e01d6ce911678a1bf89e1280aefc90140c199951b24bf5745cce54f0c448596c0176c9d052630c08c91c306fef1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ugalileo::Application.config.session_store :cookie_store, key: '_ugalileo_session'
